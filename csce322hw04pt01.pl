9347ae59b47e6c0f0760116a24b627bd746bef5b
kpagilla <kpagilla@huskers.unl.edu>
2021-04-19 02:25:51 -0500

    Updated variable names

--- csce322hw04pt01.pl
+++ csce322hw04pt01.pl
@@ -13,21 +13,22 @@ rotationsAndPlayers(Rotations,Maze):-
 % find2D(What,ListOfLists,Where)
 find2D(What,[Row|_],(0,Column)):-    
     find(What,Row,Column).    
-    find2D(What,[_|Rows],(R,C)):-    
+find2D(What,[_|Rows],(R,C)):-    
     find2D(What,Rows,(RowsR,C)),    
     R is RowsR + 1.
 
 % find(What,List,Where)    
-    find(What,[What|_],0).
-    find(What,[_|T],Where):-
+find(What,[What|_],0).
+find(What,[_|T],Where):-
     find(What,T,WhereT),    
     Where is WhereT + 1.
 
 playerCounter(Maze, Count):-
-    setof(X, (numlist(1,4,NL),
-    member(X, NL),
-    playerFound(Maze, X)), L),
-    length(L, Count).
+    setof(Player, 
+    (numlist(1, 4, PossiblePlayerList),
+    member(Player, PossiblePlayerList),
+    playerFound(Maze, Player)), PlayerList),
+    length(PlayerList, Count).
 
-playerFound(Maze, X):-
-    find2D(X, Maze, Where).
+playerFound(Maze, Player):-
+    find2D(Player, Maze, Where).
--- csce322hw04pt03.pl
+++ csce322hw04pt03.pl
@@ -5,26 +5,26 @@ notStacked(Maze):-
 % find2D(What,ListOfLists,Where)
 find2D(What,[Row|_],(0,Column)):-    
     find(What,Row,Column).    
-    find2D(What,[_|Rows],(R,C)):-    
+find2D(What,[_|Rows],(R,C)):-    
     find2D(What,Rows,(RowsR,C)),    
     R is RowsR + 1.
 
 % find(What,List,Where)    
-    find(What,[What|_],0).
-    find(What,[_|T],Where):-
+find(What,[What|_],0).
+find(What,[_|T],Where):-
     find(What,T,WhereT),    
     Where is WhereT + 1.
 
 stackedPlayerCounter(Maze, Count):-
-    findall(X-Y, (numlist(1,4,NL),
-    member(X, NL),
-    member(Y, NL),
-    X =\= Y,
-    playerRow(Maze, X, Y)), L),
-    length(L, Count).
+    findall(Player1-Player2, (numlist(1, 4, PossiblePlayerList),
+    member(Player1, PossiblePlayerList),
+    member(Player2, PossiblePlayerList),
+    Player1 =\= Player2,
+    playerRow(Maze, Player1, Player2)), PlayerList),
+    length(PlayerList, Count).
 
-playerRow(Maze, X, Y):-
-    find2D(X, Maze, (R, C)),
-    find2D(Y, Maze, (R1, C1)),
+playerRow(Maze, Player1, Player2):-
+    find2D(Player1, Maze, (R, C)),
+    find2D(Player2, Maze, (R1, C1)),
     1 is abs(R - R1),
     0 is abs(C - C1).
